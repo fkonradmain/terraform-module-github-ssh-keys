719c555e569d81382c41c2e6073030c10e0c7d0c
fkonradmain <88140888+fkonradmain@users.noreply.github.com>
2024-04-25 09:55:41 +0200

    fix/terraform version lock (#4)
    * removed terraform version constrained, this allows the use of terraform 1.8.0 and later versions
    
    * removed other version constraints as well - this makes the module most flexible
    
    ---------
    
    Co-authored-by: fabian <fabian@localhost.invalid>

--- terraform.tf
+++ terraform.tf
@@ -1,13 +1,13 @@
 terraform {
-  required_version = "~>1.7.0"
+  required_version = ">1.7.0"
   required_providers {
     github = {
       source  = "integrations/github"
-      version = "~>6.2.0"
+      version = ">6.2.0"
     }
     http = {
       source  = "hashicorp/http"
-      version = "3.4.2"
+      version = ">3.4.2"
     }
   }
 }
